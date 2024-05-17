2f59a5794a720b7b353c554ded4ab3310338d402
violecheb <163555541+violecheb@users.noreply.github.com>
2024-05-16 23:16:03 -0300

    PRACTICA 2, ME FALTA TERMINAR LA 3

--- index.js
+++ index.js
@@ -54,3 +54,119 @@ if (numero >= 1 and numero <= 7) {
 }
 
  /* practica 2 (hacer a la noche)*/
+/* 1 */
+let misDatos = {
+nombre: "Violeta",
+apellido: "Cheb",
+dni: "46025678",
+comidasFavoritas: ["helado", "hamburguesa", "tiramisu"], /* tengo que usar array */
+}
+
+console.log(misDatos);
+
+/* 2 */
+let misDatos = {
+nombre: "Violeta",
+apellido: "Cheb",
+dni: "46025678",
+comidasFavoritas: ["helado", "hamburguesa", "tiramisu"],
+saludar: function() {
+return "Hola mi nombre es " + this.nombre + " " + this.apellido + " y tengo " + this.edad + " años. Mi primera comida favorita es " + this.comidas_fav[0] + "."
+}
+}
+console.log(misDatos2.saludar());
+
+/* 3 */
+let auto = {
+marca: "Volkswagen",
+modelo: "GOL",
+anio: 2018,
+color: "Negro",
+posicion: 0,
+avanzar: function(n) {
+this.posicion += n;
+return this.posicion;
+},
+retroceder: function(n) {
+this.posicion -= n;
+return this.posicion ;
+}
+}
+
+auto.avanzar(10);
+auto.retroceder(5);
+auto.avanzar(1);
+console.log(auto.posicion); /* dentro de los parentesis de console log llamo a la posicion del auto */ 
+
+/* 4 */
+
+let nuevoAuto = {
+marca: "Volkswagen",
+modelo: "GOL",
+anio: 2018,
+color: "Negro",
+posicion: 0,
+moverse: function(n) {
+if n>0:
+ this.posicion += n;
+ return this.posicion;
+elif n<0:
+ this.posicion -= n;
+ return this.posicion ;
+}
+}
+nuevoAuto.moverse(2);
+nuevoAuto.moverse(-5);
+console.log(nuevoAuto.posicion)
+
+/* 5 */
+
+let ironMan = {
+nombre: "Iron Man",
+equipo: "Avengers",
+poderes: ["Volar", "Lanzar misiles", "Disparar láser"],
+energia: 100,
+getPoder: function(Fn) {
+this.energia -= 10;
+return "Poder elegido de " + this.nombre + ": " + this.poderes[Fn] + ". Energía restante: " + this.energia;
+}
+}
+
+let Hulk = {
+nombre: "Hulk",
+equipo: "Avengers",
+poderes: ["Aplastar", "Gritar", "Golpear"],
+energia: 100,
+getPoder: function(Fn) {
+this.energia -= 10;
+return "Poder elegido de " + this.nombre + ": " + this.poderes[Fn] + ". Energía restante: " + this.energia;
+}
+}
+console.log(ironMan.getPoder(0));
+console.log(Hulk.getPoder(2));
+
+ /* practica 3 */
+
+/* 1 */
+for (let i = 0; i <= 5; i++) {
+console.log("Practicando con el bucle for " + i);
+}
+
+for (let i = 0; i <= 10; i++) {
+console.log(`La variable i tiene el valor ${i} `);
+}
+
+for (let i = 0; i < 10; i++) {
+console.log(2 * i);
+}
+
+for (let i = 0; i <= 50; i++) {
+console.log(5 * i);
+}
+
+for (let i = 0; i <= 100; i++) {
+console.log(i);
+}
+
+ /* NO LLEGE, TERMINAR MAÑANA A LA MAÑANA*/
+
