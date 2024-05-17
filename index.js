7ba6a7a3b9282947b4cab7d3831308f64c525123
violecheb <163555541+violecheb@users.noreply.github.com>
2024-05-17 07:28:04 -0300

    PRACTICA 3 Y TERMINADO

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- /* practica 1 */
+ /* PRACTICA 1 FUNCIONES */
 /* 1 */
 function rectangulo(base, altura) {
 return base * altura;
@@ -53,7 +53,7 @@ if (numero >= 1 and numero <= 7) {
 }
 }
 
- /* practica 2 (hacer a la noche)*/
+ /* PRACTICA 2 OBJETOS LITERALES*/
 /* 1 */
 let misDatos = {
 nombre: "Violeta",
@@ -145,7 +145,7 @@ return "Poder elegido de " + this.nombre + ": " + this.poderes[Fn] + ". Energía
 console.log(ironMan.getPoder(0));
 console.log(Hulk.getPoder(2));
 
- /* practica 3 */
+ /* PRACTICA 3 BUCLES*/
 
 /* 1 */
 for (let i = 0; i <= 5; i++) {
@@ -168,5 +168,60 @@ for (let i = 0; i <= 100; i++) {
 console.log(i);
 }
 
- /* NO LLEGE, TERMINAR MAÑANA A LA MAÑANA*/
+/* 3 */
+ganancias = [60, 24, -93, -9, 33, 12, -66, -8, 90, 5];
+suma = 0;
+
+for (let i = 0; i < ganancias.length; i++) {
+suma += ganancias[i];
+}
+
+console.log(suma);
+
+/* 4 */
+
+let healinglsDifficult = [
+'Fear', 'Drink to Get Drunk', 'Taken for Granted', 'Blow It All Away', 'Get Me', 'Im Not Important to You', 'Sober and Unkissed', 'Healing Is Difficult', 'Judge Me', 'Little Man', 'Insidiously'];
+
+for (let i = 0; i <healinglsDifficult.length; i++) {
+console.log(healinglsDifficult[i]);
+}
+
+n = 0
+for (let i = 0; i < healinglsDifficult.length; i++) {
+n += 1
+console.log(n + " " + healinglsDifficult[i]);
+}
+
+/* 5 */
+let got = [
+{
+nombre: "Jon",
+apellido: "Snow",
+edad: 23,
+ciudad: "Winterfell"
+},
+{
+nombre: "Daenerys",
+apellido: "Targaryen",
+edad: 19
+},
+{
+nombre: "Arya",
+apellido: "Stark",
+edad: 12,
+ciudad: "Winterfell"
+},
+{
+nombre: "Tyrion",
+apellido: "Lannister",
+edad: 32,
+ciudad: "Casterly Rock"
+}
+]
+for (let i = 0; i < got.length; i++) {
+console.log("hola " + got[i].nombre + got[i].apellido + "criatura viajera");
+console.log("soy "+ got[i].nombre + got[i].apellido + " de la ciudad " + got[i].ciudad);
+}
+
 
